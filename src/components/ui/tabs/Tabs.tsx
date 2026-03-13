279e58b5b218ca9350c0b87b09f74f983f0f6d75
emresahin19 <81564669+emresahin19@users.noreply.github.com>
2026-03-13 21:32:09 +0300

    tabs

--- src/components/ui/tabs/Tabs.tsx
+++ src/components/ui/tabs/Tabs.tsx
@@ -5,6 +5,7 @@ import { useSelectedLayoutSegment } from 'next/navigation';
 import { clsx } from '@/utils';
 import { Tab } from './tabs.types';
 import styles from './tabs.module.scss';
+import { useEffect, useRef, useState } from 'react';
 
 interface TabsProps {
   tabs: Tab[];
@@ -13,20 +14,44 @@ interface TabsProps {
 
 export default function Tabs({ tabs, children }: TabsProps) {
   const selectedSegment = useSelectedLayoutSegment();
+  const listRef = useRef<HTMLUListElement>(null);
+
+  const [indicator, setIndicator] = useState({
+    left: '0',
+    width: '0'
+  });
+
+  useEffect(() => {
+    const activeIndex = tabs.findIndex(tab => (tab.segment ?? "overview") === (selectedSegment ?? "overview"));
+    setIndicator({
+      left: `calc(${(100 / tabs.length) * (activeIndex ?? 0)}% + 8px)`,
+      width: `calc(${100 / tabs.length}% - 16px)`
+    });
+  }, [selectedSegment]);
 
   return (
     <div className={styles.root}>
       <nav className={styles.nav}>
-        <ul className={styles.list}>
+        <ul ref={listRef} className={styles.list}>
+
+          <div
+            className={styles.slider}
+            style={{
+              left: indicator.left,
+              width: indicator.width
+            }}
+          />
+
           {tabs.map((tab) => {
             const isActive =
-              (tab.segment ?? "overview") === (selectedSegment ?? "overview");
+              (tab.segment ?? "overview") ===
+              (selectedSegment ?? "overview");
 
             return (
               <li key={tab.id} className={styles.item}>
                 <Link
                   href={tab.href}
-                  className={clsx(styles.link)}
+                  className={styles.link}
                   aria-current={isActive ? "page" : undefined}
                 >
                   {tab.label}
@@ -36,10 +61,10 @@ export default function Tabs({ tabs, children }: TabsProps) {
           })}
         </ul>
       </nav>
+
       <section className={styles.content}>
         {children}
       </section>
     </div>
-
   );
 }
